b99e5d6328e579c459c25db40e292d5b105d764d
Carson Going <117052774+GarlicBreadPog@users.noreply.github.com>
2023-02-26 21:31:34 +0000

    Changes to be committed: 	new file:   lesson.py  Changes not staged for commit: 	modified:   lesson.py

---
+++ lesson.py
@@ -0,0 +1,66 @@
+"""lesson"""
+import time
+TXT = "Even though we shouldn't always judge unrighteously, we still must judge. If we didn't do that, we couldn't know who to be friends with and seperate ourselves from, or who to help. Jesus taught how to judge righteously in this weeks scriptures"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read = input()
+TXT = "lets read some scriptures! Open up to Matthew 6:14. Anson will you read a verse? ->Press enter when finished"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read = input()
+time.sleep(1)
+TXT = "now lets go around the group. ___, will you read the next verse? ->Press enter when finished"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read =input()
+time.sleep(1)
+TXT = "Now lets go read in matthew 7:3 -> Press enter when finished"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read = input()
+TXT ="keep reading on until verse 5 -> press enter when finished"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read = input()
+TXT="In these scriptues, Jesus tells us to take the beam out of our own eye before judging the mote of others, as your view is obstructed. In other words, don't judge others for your mistakes while you are making those same ones, or bigger ones, yourself. Who has done that before? -> press enter when finished"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read = input()
+TXT="How can we improve? -> press enter when finished"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read = input()
+TXT="next, lets read Mathew 7:12 -> press enter when finished"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read = input()
+TXT = "After we finish repenting, and we are able to judge, it is easy to do it in the wrong way. How can this scripture help us do it right? -> press enter when finished"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read = input()
+TXT = "Who wants to be judged by others? -> press enter when finished"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read = input()
+TXT = "I sure don't lol. isn't it good to judge righteously? but if we don't want to be judged, isn't that contradicting what else Jesus said? People don't like judgy people, though Dallin H. Oak's Ensign article, \"'Judge Not' and Judging\" talks about this. In it, he says, \"Whenever Possible we will refrain from judging people and only judge situations. This is essential whenever we attempt to act upon different standards than others with whomever we associate- at home, at work, or in the community. We can set and act upon high standards for ourselves or our homes without condemning those who do otherwise\" In other words, even once we remove our beam out of our eye, instead of judging people, judge the situations surrounding them when possible. How can we follow this message in our daily lives?"
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
+read = input()
+TXT="""Finally, I'd like you to ponder the impressions you've felt today, and think about how you can act on them.
+Write this down, so you can remember it and be reminded of it. It will bring you closer to God as you do it.
+I Bear my testimony that I know this church is true, and that if we will do these things we will become closer to God
+and I say these things in the name of jesuc christ, Amen."""
+for char in TXT:
+    print(char, end="", flush=True)
+    time.sleep(.025)
